693d81621efa62f6a1b1f16b2845633ac326f6ac
Gen Taliaru <gen@infira.ee>
2022-11-27 16:44:00 +0200

    v2 dev

--- test/index.php
+++ test/index.php
@@ -1,23 +1,37 @@
 <?php
 
 use Infira\Error\Handler;
+use Infira\Error\Error;
 
 require_once "../vendor/autoload.php";
-Handler::register();
+Handler::register([
+    'errorLevel' => -1,
+    'dateFormat' => 'd.m.Y H:i:s'
+]);
 try {
-	\Infira\Error\Error::addDebug('global debug data', 'random string');
-	asdasds("trigger_error");
-	exit;
-	addExtraErrorInfo('more', ['value1', 'value2']);
-	alert('my custom error', ['extra' => 'data']);
-	exit;
-	//echo $aas;// addExtraErrorInfo("extraData", "extra data value");
-	throw new Exception('error');
-	//\Infira\ErrorException\Handler::raise("Raise infira error");
-	//raiseSomeError();
-	//trigger_error("error");
+    Error::capsule(function () {
+        Error::setDebug(['blaah' => "asdasd"]);
+        //none
+    });
+
+    throw Error::RuntimeException('aasd')->withDebug([
+        'global debug data' => 'random string'
+    ]);
+
+
+    Error::setDebug('global debug data', 'random string');
+    asdasds("trigger_error");
+    exit;
+    addExtraErrorInfo('more', ['value1', 'value2']);
+    alert('my custom error', ['extra' => 'data']);
+    exit;
+    //echo $aas;// addExtraErrorInfo("extraData", "extra data value");
+    throw new Exception('error');
+    //\Infira\ErrorException\Handler::raise("Raise infira error");
+    //raiseSomeError();
+    //trigger_error("error");
 }
 catch (Throwable $e) {
-	echo "<pre>";
-	echo print_r(Handler::compile($e)->toArray());
+    echo "<pre>";
+    echo print_r(Handler::compile($e)->toArray());
 }
